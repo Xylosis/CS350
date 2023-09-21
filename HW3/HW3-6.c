81cc987922be0c1dfd70d8fae23caacb1d5a5ee0
Xylosis <terminatordog0000@gmail.com>
2023-09-21 16:21:30 -0400

    lol

--- HW3/HW3-6.c
+++ HW3/HW3-6.c
@@ -10,46 +10,38 @@ float strToDecimal(char s[LEN]) {
    unsigned int uInt = 0;
    unsigned int mask;
 
+    printf("binary value = ");
+    for(int i = 0; i < LEN-1; i++) {
+        printf("%c", s[i]);
+        if(i == 0 || i == EXP_32)
+            printf(" ");
+    }
+    printf(" --> ");
     for (int i = 0; i < (LEN); i++) {
-        if(s[i] == ' ')
+        if (s[i] == ' ')
             continue;
         if(s[i] == '1') {
             uInt = (uInt | 1 << (31 - i));
         }
     }
+    // mask = 1 << 31;
+    // for(int i = 0; i < LEN-1; i++) {
+    //     printf("%u", (uInt & mask)!=0?1:0);
+    //     mask = mask >> 1;
+    // }
 
-
-    int flag = 0;
-    mask = 1 << 31;
-    for(int i = 0; i < LEN; i++) {
-        flag = 0;
-        printf("%u", (uInt & mask)!=0?1:0);
-        mask = mask >> 1;
-    }
-    printf("\n");
-    printf("uInt = %u\n", uInt);
     float f = *((float *) &uInt);
-    printf("%f\n", ((float *)&uInt));
-    printf("%f", f);
     return f;
 }
 
 
 int main(int argc, char **argv) {
   int n=LEN;
-  char s[LEN];
+  char s[LEN] = "001111110100000000000000000000";
 
-  printf("binary value = ");
-  for (int i = 0; i < (n); i++) {
-    s[i] = argv[1][i];
-    printf("%c", s[i]);
-    if(i == EXP_32 || i == 1)
-        printf(" ");
-  }
-  printf("\n");
-  float f = strToDecimal(s);
-
-  printf("float = %f\n", f);
+  printf("float value = %f\n", strToDecimal(s));
+  
+  printf("float value = %f\n", strToDecimal("00111110000000000000000000000000"));
 
   return 0;
 }
